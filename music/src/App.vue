989c4726ce1e6ce45f89a18ef45b35092bb00ad2
cmq <1791194092@qq.com>
2022-11-07 16:48:23 +0800

    music

---
+++ music/src/App.vue
@@ -0,0 +1,27 @@
+
+
+<template>
+   <el-button type="primary" @click="aa">主要按钮</el-button>
+</template>
+<script >
+   export default{
+    data() {
+      return {
+        
+      }
+    },
+    created(){
+      alert(77)
+    },
+    methods:{
+      aa(){
+        alert(66)
+      }
+    }
+
+
+   }
+</script>
+<style scoped>
+
+</style>
